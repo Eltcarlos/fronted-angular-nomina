b3922718c0ef3f8a24e1815e97b72995ad640d6c
Carlos Diaz <carlos.diaz@smartsoft.com.co>
2023-08-28 23:28:27 -0500

    add new env

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://backend-nomina.onrender.com',
+  API_URL: 'https://app-nomina-a203449a9fac.herokuapp.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'https://backend-nomina.onrender.com',
+  API_URL: 'https://app-nomina-a203449a9fac.herokuapp.com',
 };
